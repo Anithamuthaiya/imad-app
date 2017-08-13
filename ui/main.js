32af905d669a4de0432940c237e5dad65499346b
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-14 02:32:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,16 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML ='New value';
 
-var img = document.getElementById('madi');
-var marginleft=0;
-function moveRight()
-{
-    marginleft=marginleft+10;
-    img.style.marginleft=marginleft + 'px' ;
-}
+//var img = document.getElementById('madi');
+//var marginleft=0;
+//function moveRight()
+//{
+//    marginleft=marginleft+10;
+//    img.style.marginleft=marginleft + 'px' ;
+//}
 
 img.onclick=function()
 {
-var interval=setInterval(moveRight, 100);
+    img.style.marginleft='100px';
+//var interval=setInterval(moveRight, 100);
 };

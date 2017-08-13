829bd4b549030b3bfc774c396306120d0264c356
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-14 02:17:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 
 var element= document.getElementById('main-text');
 element.innerHTML ='New value';
+
+var img = document.getElementById('modi');
+var marginleft=0;
+function MoveRight(){
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft;
+}
+img.onclick=function(){
+var interval=SetInterval(MoveRight,100);
+}

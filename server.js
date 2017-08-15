b11f0ef7be580542081828e9ec3b24ba164cdf69
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-16 01:00:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ function createTemplate(data)
 <div>
 <div>
 <div>
-<textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">
+<textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;background-color:gold">
 Hey... say something!
 </textarea>
 </div>

3ac3240146f39674e03700fec07d308f039ec363
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-15 06:40:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,17 +108,18 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+counter=0;
+app.get('/counter',function(req,res) {
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/:articleName',function(req,res){
 var articleName=req.params.articleName;
 res.send( createTemplate(articles[articleName]));
 });
 
-counter=0;
-app.get('/counter',function(req,res) {
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 
 

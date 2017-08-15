0fbd9b2b1a0444377b21c5f6306045882f479cba
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-15 17:10:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,15 +25,15 @@ button.onclick=function()
     {
         var names=['name1','name2','name3','name4'];
         var list='';
-        for(var i=0;i<names.length;<i++)
+        for(var i=0; i<names.length; <i++)
         {
-            list+='<li>'+name[i]+'</li>';
+            list += '<li>'+names[i]+'</li>';
             
         }
-        
-    var ul=document.getElementById(namelist);
-        ul.innerHTML=list
-};
+         var ul =document.getElementById('namelist');
+            ul.innerHTML=list;
+    };
+   
 
 
 

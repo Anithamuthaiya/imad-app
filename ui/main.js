9d34a396fe7a7fa7b9a98c341a6eea5854e73fee
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-15 06:49:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 //console.log('Loaded!');
 
 var button= document.getElementById('counter');
-//var counter=0;
-//button.onclick=function()
+var counter=0;
+button.onclick=function()
 {
     counter=conter+1;
     var span=document.getElementById('count');

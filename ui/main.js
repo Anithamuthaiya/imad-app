1ed0eaddf025e2800e3745bda738df5770f59604
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-15 07:38:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
 //console.log('Loaded!');
 
 var button= document.getElementById('counter');
-var counter=0;
-button.onclick = function()
+button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=XMLHTTPRequest();
+    request.onreadystatechange=funtion(){
+        if(request.readystate==XMLHTTPRequest.DONE){
+            if(request.status==200){
+                var counter=request.resonseText;
+                var span=document.getElementById('counter');
+                span.innerHTML==counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/counter',true);
+    request.send(null);
 };

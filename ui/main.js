c1d040bb7bc0a330b523a20b44b56f523c27ae1b
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-16 01:18:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,34 @@ submit.onclick=function()
     
     
     
+    var submit=document.getElementById('comment');
+submit.onclick=function()
+{
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var comment=request.responseText;
+                comments=JSON.parse(comments);
+                 var list='';
+                 for(var i=0; i<comments.length; i++)
+        {
+            list += '<li>'+comments[i]+ '</li>';
+            
+        }
+         var ul =document.getElementById('commentlist');
+            ul.innerHTML=list;
     
+               
+            }
+        }
+        
+    };
+    var commentInput=document.getElementById('comment');
+    var comment=commentInput.value;
+    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/submit?comment='+comment,true);
+    request.send(null);
+}; 
     
   
 

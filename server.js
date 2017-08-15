8da689cc0e6df5056d6d4dd7f5aaee79174ff7ab
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-15 23:46:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/submit-name/:name', function(req,res){
     var name=req.params.name;
     names.push(name);
     
-res.send(JSON,stringify(names));
+res.send(JSON.stringify(names));
 });
 
 

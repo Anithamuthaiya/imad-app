b047cc9cbf228fcce0cfae8a9bfd466a4a132676
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-24 01:37:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto=require('crypto');
+var bodyParer=require('body.parser');
 
 var app = express();
 var articles={ 
@@ -126,6 +127,7 @@ return htmltemplate;
 
 
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -172,6 +174,25 @@ app.get('/hash/:input',function(req,res)
 {
     var hashedString=hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
+});
+
+
+app.post('/create-user',function(req,res)
+{
+    var username=req.body.username;
+     var password=req.body.password;
+     var salt=crypto.getRandomBytes(128).toString('hex');
+     var dbString=hash(password,salt);
+     pool.query('INSERT INTO "user"(username,password)VALUES($1,$2)',[username,dbString],function(err,result)
+     {
+         if(err){
+             res.status(500).send(err.toString());
+             }
+             else
+             {
+                 res.send('user created successfully'+username);
+             }
+             });
 });
 
 app.get('/ui/style.css', function (req, res) {

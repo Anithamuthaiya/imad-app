050730aaeae1fd179d3d96d0df8838cc8e8b59a2
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-29 14:19:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -77,7 +77,7 @@ submit.onclick=function()
     };
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
-    request.open('POST','http://anithamuthaiya.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 }; 
     

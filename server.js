d0db411b6ff2a6a4739854de4c91e051db320e07
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-29 19:13:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser=require('body-parser');
 var config = {
     user:'anithamuthaiya',
     database:'anithamuthaiya',
-    host:'db.imad.hosura-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };

75d60e72d57543b1ebaf33cc6d93bc4a010c6df9
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-08-30 16:54:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = {
     database:'anithamuthaiya',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:db-anithamuthaiya-50927
+    password:process.env.DB_PASSWORD
 };
 
 

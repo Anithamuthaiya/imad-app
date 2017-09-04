af73bbe8cb88732924be00c5740b64bcad200be6
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-09-04 23:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,10 @@ var articles={
      }
      
 };
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
 function createTemplate(data)
 {
     var title=data.title;
@@ -138,12 +142,123 @@ return htmltemplate;
 }
 
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
+app.get('/', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 
-var pool=new Pool(config);
-app.post('/create-user',function(req,res)
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+counter = counter + 1;
+res.send(counter.toString());
+});
+
+
+
+var names = [];
+app.get('/submit-name',function(req,res){
+var name= req.query.name;
+names.push(name);
+res.send(JSON.stringify(names));
+});
+
+
+
+var pool =new Pool(config);
+app.get('/test-db', function (req, res) {
+pool.query('SELECT * FROM test', function(err, result){
+if(err){
+res.status(500).send(err.toString());
+}else{
+res.send(JSON.stringify(result.rows));
+}
+}); 
+});
+
+app.get('/articles/:articleName', function (req, res) {
+pool.query("SELECT * FROM article WHERE title =$1" ,[req.params.articleName], function(err, result){
+if(err){
+res.status(500).send(err.toString());
+}else{
+if(result.rows.length === 0){
+res.status(404).send('Article not found');
+}else{
+var articleData = result.rows[0];
+res.send(createTemplate(articleData));
+}
+}
+}); 
+});
+
+function hash(input,salt){
+var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+app.get('/hash/:input',function(req,res){
+var hashedString = hash(req.params.input,'this-is-some-random-string');
+res.send(hashedString);
+
+});
+
+app.post('/create-user',function(req,res){
+var username= req.body.username;
+var password = req.body.password;
+var salt = crypto.randomBytes(128).toString('hex');
+var dbString = hash(password,salt);
+pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+if(err){
+res.status(500).send(err.toString());
+}else{
+res.send('user successfully created: ' + username);
+}
+});
+});
+app.post('/login',function(res,req){
+var username = req.body.username;
+var password = req.body.password;
+
+pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err,result){
+     if(err){
+      res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length === 0){
+            res.send(403).send('username/password is invalid');
+        }
+        else{
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt);
+            if(hashedPassword === dbString){
+                res.send('credentials correct!');
+            }
+            else{
+                res.send(403).send('username/password is invalid');
+            }
+
+        }
+
+    }
+});
+});
+app.get('/ui/style.css', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/madi.png', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+var port = 80;
+app.listen(port, function () {
+console.log(IMAD course app listening on port ${port}!);
+});
+
+
+/*app.post('/create-user',function(req,res)
 {
     
     var username=req.body.username;
@@ -165,8 +280,10 @@ app.post('/create-user',function(req,res)
 app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+*/
 
-function hash(input,salt){
+
+/*function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return["pbkf2Sync","10000",salt,hashed.toString('hex')].join('$');
 }
@@ -236,4 +353,4 @@ app.get('/ui/main.js', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-});
+});*/

28a2c98faf6c056437d4f140815ecd1f5282ab9c
Anithamuthaiya <30739246+Anithamuthaiya@users.noreply.github.com>
2017-09-05 00:07:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,84 +1,31 @@
 //console.log('Loaded!');
 
-var button= document.getElementById('counter');
-button.onclick=function()
-{
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('counter');
-                span.innerHTML=counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/counter',true);
-    request.send(null);
-}; 
-    
-  
-  
-  
-   
-    var submit=document.getElementById('submit_btn');
+var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
     var request= new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-                var name=request.responseText;
-                name=JSON.parse(name);
-                 var list='';
-                 for(var i=0; i<name.length; i++)
-        {
-            list += '<li>'+name[i]+ '</li>';
-            
-        }
-        
-          ul =document.getElementById('namelist1');
-            ul.innerHTML=list;
-    
-               
-            }
+                console.log('user logged in');
+                alert('user logged sucessfully');
+                }
+                else if(request.status === 403){
+                    alert('username/password is incorrect');
+                }
+                else if(request.status === 500){
+                    alert('something went wrong on system');
+                }
         }
         
     };
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/submit1-name?name='+name ,true);
-    request.send(null);
-};
-    
-    var submit=document.getElementById('submit_bn');
-submit.onclick=function()
-{
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var names=request.responseText;
-                names=JSON.parse(names);
-                 var list='';
-                 for(var i=0; i<names.length; i++)
-        {
-            list += '<li>'+names[i]+ '</li>';
-            
-        }
-         var ul =document.getElementById('namelist');
-            ul.innerHTML=list;
-    
-               
-            }
-        }
-        
-    };
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET','http://anithamuthaiya.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+     var password=document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+    request.open('POST','http://anithamuthaiya.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
 }; 
     
     
